deb8f39b0b3c2ea33d1e42e492273b9b2a915d1f
FriskyMeek <michaelpeacock1993@gmail.com>
2025-06-18 02:45:55 +0100

    Initial HyperRoute project setup

---
+++ src/AudioEngine/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AudioEngine
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("HyperRoute Audio Engine v1.0");
+            Console.WriteLine("Available commands: list-devices, list-sessions, route");
+            
+            if (args.Length > 0) 
+            {
+                Console.WriteLine($"Command received: {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("No command specified. Use: AudioEngine.exe [command]");
+            }
+        }
+    }
+}
